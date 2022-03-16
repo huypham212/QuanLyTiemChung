d75f7c18d37c9529c97b5ae77fe0eed059eb6f2a
huypham2120 <thanhhuy2120@gmail.com>
2022-03-16 07:49:00 +0700

    finish UI screen VaccineCalendar

---
+++ Mobile/tiemchung/src/views/VaccineCalendar/styles.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    titleStyle: {
+        fontSize: 35,
+        fontWeight: 'bold',
+        justifyContent: 'center',
+        textAlign: 'center'
+    },
+
+    subTitleStyle: {
+        fontSize: 15,
+        justifyContent: 'center',
+        textAlign: 'center'
+    },
+
+    dividerStyle: {
+        marginLeft: -5,
+        marginRight: 15
+    },
+
+    cardContainer: {
+        borderRadius: 10,
+        flexDirection: 'row'
+    },
+
+    cardContentLeft: {
+        marginRight: 25,
+    },
+
+    cardContentRight: {
+        flexDirection: 'row',
+        marginBottom: 15,
+    },
+
+    iconStyles: {
+        marginRight: 10
+    },
+
+    textStyles: {
+        fontSize: 18,
+    }
+
+})
+
+export default styles
