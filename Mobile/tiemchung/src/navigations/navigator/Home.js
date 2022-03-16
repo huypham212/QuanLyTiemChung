6dce24a90aacf7037b042b4bb5e08c3e5eddddbf
huypham2120 <thanhhuy2120@gmail.com>
2022-03-16 07:48:20 +0700

    add header to some screen

--- Mobile/tiemchung/src/navigations/navigator/Home.js
+++ Mobile/tiemchung/src/navigations/navigator/Home.js
@@ -3,14 +3,15 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from "../../views/Home/index";
 import InfoScreen from '../../views/InfoScreen/index';
 import SettingScreen from '../../views/SettingScreen/index';
+import VaccineCalendar from '../../views/VaccineCalendar/index';
 
 const Stack = createNativeStackNavigator()
 const HomeNavigator = () => {
-    //console.log("Home");
     return (<Stack.Navigator>
         <Stack.Screen name='Home' component={HomeScreen} options={{ headerShown: false }}></Stack.Screen>
-        <Stack.Screen name='Info' component={InfoScreen} options={{ headerShown: false }}></Stack.Screen>
+        <Stack.Screen name='Info' component={InfoScreen} options={{ title: "Thông tin" }}></Stack.Screen>
         <Stack.Screen name='Setting' component={SettingScreen} options={{ headerShown: false }}></Stack.Screen>
+        <Stack.Screen name='Calendar' component={VaccineCalendar} options={{ title: "Danh sách đăng ký tiêm" }}></Stack.Screen>
     </Stack.Navigator>)
 }
 
