fb8bc22d1a5fe66afde0fdf5c001409b8d5cbe6f
huypham2120 <thanhhuy2120@gmail.com>
2022-03-17 11:35:52 +0700

    reconfig firebase with database

--- Mobile/tiemchung/src/firebase/config.js
+++ Mobile/tiemchung/src/firebase/config.js
@@ -4,6 +4,7 @@ import { initializeApp } from "firebase/app";
 const firebaseConfig = {
     apiKey: "AIzaSyDVTE1TX-FBbK-gH-k9_BtxsSMt8leH288",
     authDomain: "qltc-57440.firebaseapp.com",
+    databaseURL: "https://qltc-57440-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "qltc-57440",
     storageBucket: "qltc-57440.appspot.com",
     messagingSenderId: "173278656065",
