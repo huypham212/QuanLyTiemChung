fb552c0d35ead985dcc9cba2c6502127813b7b9a
huypham2120 <thanhhuy2120@gmail.com>
2022-03-30 10:14:36 +0700

    set loading for button in VaccineInfo

--- Mobile/tiemchung/src/views/InfoScreen/VaccineInfo.js
+++ Mobile/tiemchung/src/views/InfoScreen/VaccineInfo.js
@@ -9,20 +9,23 @@ import styles from './styles';
 const VaccineInfo = () => {
 
     const [injectedData, setInjectedData] = useState([]);
-    console.log("First: ", injectedData);
-
-    useEffect(() => {
-        database().ref("/injectedInfo/" + auth().currentUser.uid).on('value', snapshot => {
-            setInjectedData(snapshot.val())
-        })
-        console.log("Second", injectedData)
-    }, [])
+    const [isLoad, setIsLoad] = useState(false);
 
     const fetchData = () => {
+        setIsLoad(true);
         database().ref("/injectedInfo/" + auth().currentUser.uid).on('value', snapshot => {
             setInjectedData(snapshot.val())
+            setIsLoad(false)
         })
     }
+
+    useEffect(() => {
+        fetchData();
+
+        return () => {
+            setInjectedData([]);
+        }
+    }, [])
 
     return (
         <ScrollView>
@@ -34,30 +37,39 @@ const VaccineInfo = () => {
                     type: "font-awesome-5",
                     size: 20,
                 }}
-                loading
+                loading={isLoad}
                 iconPosition='left'
                 titleStyle={{ fontSize: 20 }}
                 buttonStyle={styles.btnStyle}
                 onPress={() => fetchData()}
             />
-            {injectedData.map((l, i) => (
-                <Card key={i} containerStyle={styles.cardTopContainer}>
-                    <Card.Title style={styles.titleStyle}>Mũi {i + 1}</Card.Title>
-                    <Card.Divider width={1.5} />
-                    <Text style={styles.contentStyle}>Tên Vaccine:
-                        <Text style={styles.detailStyle}> {l.vaccineName}</Text>
-                    </Text>
-                    <Text style={styles.contentStyle}>Ngày tiêm:
-                        <Text style={styles.detailStyle}> {l.injectedDate}</Text>
-                    </Text>
-                    <Text style={styles.contentStyle}>Số lô vaccine:
-                        <Text style={styles.detailStyle}> {l.batchNumber}</Text>
-                    </Text>
-                    <Text style={styles.contentStyle}>Cơ sở tiêm:
-                        <Text style={styles.detailStyle}> {l.injectedLocation}</Text>
-                    </Text>
-                </Card>
-            ))}
+            {injectedData == null ? (
+                <View>
+                    <Text>Bạn chưa tiêm mũi nào</Text>
+                </View>
+            ) : (
+                <View>
+                    {injectedData.map((l, i) => (
+                        <Card key={i} containerStyle={styles.cardTopContainer}>
+                            <Card.Title style={styles.titleStyle}>Mũi {i + 1}</Card.Title>
+                            <Card.Divider width={1.5} />
+                            <Text style={styles.contentStyle}>Tên Vaccine:
+                                <Text style={styles.detailStyle}> {l.vaccineName}</Text>
+                            </Text>
+                            <Text style={styles.contentStyle}>Ngày tiêm:
+                                <Text style={styles.detailStyle}> {l.injectedDate}</Text>
+                            </Text>
+                            <Text style={styles.contentStyle}>Số lô vaccine:
+                                <Text style={styles.detailStyle}> {l.batchNumber}</Text>
+                            </Text>
+                            <Text style={styles.contentStyle}>Cơ sở tiêm:
+                                <Text style={styles.detailStyle}> {l.injectedLocation}</Text>
+                            </Text>
+                        </Card>
+                    ))}
+                </View>
+            )}
+
         </ScrollView>
     )
 }
