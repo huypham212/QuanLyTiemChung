4473d14621dc149af4858b1bb506c437fc44ae6f
huypham2120 <thanhhuy2120@gmail.com>
2022-03-30 23:48:15 +0700

    remove comments in index.js in redux root folder

--- Mobile/tiemchung/src/redux/index.js
+++ Mobile/tiemchung/src/redux/index.js
@@ -1,9 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import asyncThunk from 'redux-async-thunk';
 import appReducer from './reducers';
-//import { getUser } from '../middleware';
-
-//const middlewareEnhancer = applyMiddleware(getUser);
 
 const rootReducer = combineReducers(
     { appState: appReducer }
