9af52b28bd05533454d44e77f43e1cde0991ac1d
huypham2120 <thanhhuy2120@gmail.com>
2022-04-01 09:53:52 +0700

    add injectedRegistration screen

---
+++ Mobile/tiemchung/src/views/injectedRegistration/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./styles";
+import { ScrollView } from "react-native";
+import { Button, Text, InputField } from "../../components";
+
+const injectedRegistration = () => {
+    return (
+        <ScrollView>
+            <Text>ABC</Text>
+        </ScrollView>
+    )
+}
+
+export default injectedRegistration
