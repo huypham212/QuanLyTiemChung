d6d742b785ec0205e11ff0a5b1eae0f93f3e30e6
huypham212 <thanhhuy2120@gmail.com>
2022-06-03 11:05:09 +0700

    remove current project BE

--- BE/tiemchung/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Button } from './Button';
-export { default as TextField } from './TextField';
-export { default as DropdownMenu } from './Dropdown';
--- BE/tiemchung/src/utils/index.js
+++
@@ -1,4 +0,0 @@
-export const Validation = {
-    email: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-    password: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/g,
-};
